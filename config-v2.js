e1064f091950085fd2c7dbc31a2ae966b275a9a8
SHAGEE-MD <SHAGEEBOT@hotmail.com>
2025-02-27 19:07:09 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "LGmqvqqZFnhZHrG7IlzEiVi7S4zdvl4ZRXk0"
+const BOT_NUMBER = "94740433291"
+const SESSION_ID = "PRABATH-MD~CeIhEThZ#lSPJ-lMbJUIZu5p0fkjFSeX8E_rE17mvPXlwlIkBxY8"
 
 
 module.exports = {
